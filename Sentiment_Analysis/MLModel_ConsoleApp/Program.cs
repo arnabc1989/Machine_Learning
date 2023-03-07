fb30da6dbe9f940e95ff79567c09c975fb790c4f
arnabc1989 <96055249+arnabc1989@users.noreply.github.com>
2023-03-08 02:11:36 +0530

    Add project files.

---
+++ Sentiment_Analysis/MLModel_ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using MLModel_ConsoleApp;
+using System;
+// Add input data
+var sampleData = new MLModel.ModelInput()
+{
+    Col0 = "This restaurant was wonderful."
+};
+
+// Load model and predict output of sample data
+var result = MLModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
